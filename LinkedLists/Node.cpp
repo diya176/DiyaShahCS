1b309da495aac5ace5060ed1476d39f89609dd90
U-BSD\419842 <419842@SHS-F4W2NW3.beaverton.k12.or.us>
2024-11-12 13:13:46 -0800

    Started Linked Lists. Finished Node.h and Node.cpp

---
+++ LinkedLists/Node.cpp
@@ -0,0 +1,27 @@
+#include "Node.h"
+
+Node::Node(Student *newStudent){
+  value=newStudent;
+  next=NULL; 
+}
+
+Node::~Node(){
+  delete &value;
+  next=NULL;
+}
+
+void Node::setStudent(Student *newStudent){
+  value=newStudent;
+}
+
+char* Node::getStudent(){
+  //return value;
+}
+
+void Node::setNext(Node* newnext){
+  next=newnext;
+}
+
+Node*Node::getNext(){
+  return next;
+}
---
+++ LinkedLists/Node.h
@@ -0,0 +1,25 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+
+
+using namespace std;
+
+class Node{
+ public:
+  Node(Student*newStudent);
+  ~Node();
+  void setStudent (Student* newStudent);
+  char* getStudent();
+  void setNext(Node* newnext);
+  Node* getNext();
+
+ private:
+  Student* value;
+  Node* next;
+
+
+}
+
+#endif
