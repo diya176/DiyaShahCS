9d55e5190a18acc75632b81ee682611574934216
U-BSD\419842 <419842@SHS-F4W2NW3.beaverton.k12.or.us>
2024-12-16 12:46:43 -0800

    Finished HashTable with comments

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -7,14 +7,15 @@
 #include <string>
 using namespace std;
 
-/*This program includes an empty vector of structs to which youu can add,delete, or print students.
-To ADD a student type ADD
+/*This program makes use of a hashtable to which you can add, generate , delete, or print students.
+To ADD a student type ADD (this is manual add not generate)
+To GENERATE a student type GENERATE (this will add based on the first name and last name files). This is an easy way to check if rehasing is working.
 To DELETE a student type DELETE and then enter the id of the student
-To PRINT the current students in the vector type PRINT
+To PRINT the current students in the hashtable type PRINT
 
 
 Author: Diya Shah
-Date: 9/17/2024
+Date: 12/16/2024
  */
 
 //struct of Students with firstname, lastname, id, and gpa
@@ -29,7 +30,7 @@ struct Students
   Students* next = NULL;
 };
 
-
+//function prototypes 
 void AddName(Students** &hashtable, int &size);
 void GenerateName(Students** &hashtable, int &size);
 Students** add(Students* student, Students** &hashtable, int &size);
@@ -42,7 +43,9 @@ int main()
   //vector declaration
   //this is the vector that will be passed everywhere
   int size=100;
+  //this is the hash table 
   Students** hashtable = new Students*[size];
+  //this is for the random generators later - only needs to be set once at the beginning of a program
   srand(time(NULL));
   while (true)
   {
@@ -80,6 +83,8 @@ int main()
 }
 
 
+//manually adding a new student
+//same as student list 
 void AddName(Students** &hashtable, int &size)
 {
   //inputting of all the variables needed to add a new person 
@@ -101,21 +106,30 @@ void AddName(Students** &hashtable, int &size)
   
 }
 
+//adds a student using randomly generated data from input files 
 void GenerateName(Students** &hashtable, int &size){
   cout<<"How many students would you like to add?"<<endl;
   int students;
   cin>>students;
+  //run a for loop for the amount of students that need to be added 
   for (int i=0; i<students; i++){
     //ifstream myfile;
     //myfile.open("FirstName.txt");
+
+    //First name file was taken from https://github.com/dominictarr/random-name/blob/master/first-names.txt
     ifstream f("FirstName.txt");
+    //pick a random number from 0 to 427 because that is how many lines are in the first name file
     int num=rand()% 427;
     char firstname[100];
     for (int i = 1; i <= num; i++){
+      //get the line that corresponds to that num
+      //copy that line into the firstname char array of size firstname 
       f.getline(firstname,sizeof(firstname));
     }
     f.close();
 
+    //last name file was taken from Helen Wang's Hash Table Last Name file
+    //same logic for last name 
     ifstream l("LastName.txt");
     int num2=rand()% 441;
     char lastname[100];
@@ -127,45 +141,53 @@ void GenerateName(Students** &hashtable, int &size){
 
     
     //cout<<firstName;
+    //make a new student 
     Students* student= new Students();
+    //assign the first name, last name, id, and gpa to the respective fields 
     strcpy(student->first_name,firstname);
     cout<<student->first_name;
     strcpy(student->last_name,lastname);
     cout<<student->last_name;
+    // set the id to a random number
     int id= rand()% 100001;
     student->id=id;
+    //set the float to a random double with 2 decimal places
     float gpa = (double)(rand()%50)/10;
     student->gpa = gpa;
+    //add to the table and check for collisions and rehash if needed
     hashtable = add(student, hashtable, size);
   }
 }
 
-
+//function to check for collisions and handles any reshasing that may be necessary 
 Students** add(Students* student, Students** &hashtable, int &size) {
+  //current spot is the students hash 
   Students* current = hashtable[student->hash];
   int collisions = 0;
-
+  
+  // if there is nothing in the current space
   if(current == NULL){    
-
+    //add the student to that hash spot 
     hashtable[student->hash] = student;
   }
   //if something is already there
 
   else{
     
-    
+    //while the spot is not empty (meaning there is a collision)
     while(current->next!=NULL){
+      //keep going to the next spot
       current = current->next;
-      
+      //update collisions 
       collisions++;
-      cout<<"A collision happened"<<endl;
+      //cout<<"A collision happened"<<endl;
     }
     
     current->next = student;
   }
   
   if (collisions==3){
-    cout<<"3 collisions happened! Rehashing the table :)"<<endl;
+    //cout<<"3 collisions happened! Rehashing the table :)"<<endl;
     Students** newTable = new Students*[size*2]();
     for (int i=0;i<size;i++){
       Students* currenttemp= hashtable[i];
@@ -221,24 +243,26 @@ Students** deleteName(Students** &hashtable, int &size)
   for (int i=0; i<size; i++){
     if(hashtable[i]!=NULL){
       //cout<<i<<"here"<<endl;
+      //first student from which the linked list is created
+      //set the currrent student to the first hashtable spot
       Students* current= hashtable[i];
       Students* prev=NULL;
+      //if current is not NULL which means the spot is filled 
       while (current!=NULL){
+	//if the id is the same as the inputted id
 	if(current->id==input){
 	  //hashtable[i]=NULL;
+	  //if the previous one has no space 
 	  if (prev==NULL){
 	    hashtable[i]=current->next;
 	  }
 	  else{
 	    prev->next=current->next;
 	  }
-	  //current=NULL;
-	  //current=current->next;
-	  //cout<<current->first_name<<endl;
-	  //cout<<current->last_name<<endl;
 	  return hashtable; 
 	  //main();
 	}
+	//move to the next one
 	prev=current;
 	current=current->next;
       }
@@ -247,21 +271,27 @@ Students** deleteName(Students** &hashtable, int &size)
       ;
     }
   }
+  //return the new hashtable
   return hashtable;
 }
 
 
 void printName(Students** &hashtable, int &size)
 {
+  //go through all the table slots
   for (int i=0; i<size; i++){
+    //if there is something in that slot (aka the first person)
     if(hashtable[i]!=NULL){
       //cout<<i<<"here"<<endl;
-      cout<<hashtable[i]->first_name<<" "<<hashtable[i]->last_name<<endl;
+      //print that person's information
+      //this only works for the first person because the rest is a linked list of that first person
+      cout<<hashtable[i]->first_name<<" "<<hashtable[i]->last_name<<","<<hashtable[i]->id<<","<<hashtable[i]->gpa<<endl;
       //cout<<hashtable[i]->last_name<<endl;
       Students* current= hashtable[i];
       while (current->next!=NULL){
         current=current->next;
-        cout<<current->first_name<<endl;
+	//now keep moving through the linked list and print out all the information
+	cout<<current->first_name<<" "<<current->last_name<<","<<current->id<<","<<current->gpa<<endl;
       }
     }
     else{
