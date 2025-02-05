6de915ecdb42cc514fe4a9421603468d897ed381
U-BSD\419842 <419842@SHS-B4W2NW3.beaverton.k12.or.us>
2025-02-05 10:09:41 -0800

    heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -6,19 +6,105 @@ using namespace std;
 void add (int* heap, int &size, int numtoadd);
 void heapify (int* heap, int index);
 void print (int* heap,int pos, int depth, int size);
-void removeAll(int* heap, int index,int&size);
+int removeAll(int* heap, int index);
 
 
 int main(){
-  int heap[10]={20,10,15,7,2,11,13};
-  int size=7;
-  int numtoadd=5;
-  add(heap, size, numtoadd);
-  numtoadd=2;
-  add(heap, size, numtoadd);
-  print (heap,0,0,size);
-  removeAll(heap,0,size);
+  int heap[100];
+  int size=0;
+
+  for(int i = 1; i < 100; i++) {
+    heap[i] = 0;
+  }
+  //heapify(heap, size-1);
+  int numtoadd=20;
+  add(heap,size,numtoadd);
   print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+  
+  numtoadd=10;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+  numtoadd=15;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+  numtoadd=7;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+  numtoadd=2;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+  /*
+  numtoadd=1;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+  */
+
+  numtoadd=11;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+
+  
+  numtoadd=13;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+ 
+  numtoadd=3;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+
+  
+  numtoadd=6;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+  numtoadd=9;
+  add(heap,size,numtoadd);
+  print(heap,0,0,size);
+  cout<<endl;
+  cout<<endl;
+
+
+  int index=0;
+  char input[200];
+  cin>>input;
+  cout<<input<<endl;
+  
+  if(strcmp(input,"Largest")==0){
+    cout<<"got here"<<endl;
+    cout<<removeAll(heap,index)<<endl;
+  }
+  
+  else if (strcmp(input,"All")==0){
+    while(heap[index] != 0) {
+      cout<<removeAll(heap, index)<<endl;
+    }
+  }
+  //removeAll(heap,size,size);
+  //removeAll(heap,size,size);
+  //print(heap,0,0,size);
 }
 
 void add (int* heap, int &size, int numtoadd){
@@ -36,9 +122,11 @@ void heapify (int* heap, int index){
     return;
   }
   else{
+    //might be parent index;
     swap(heap[parent],heap[index]);
     heapify(heap,parent);
   }
+  
 }
 
 
@@ -56,39 +144,50 @@ void print (int* heap,int pos, int depth, int size){
 }
 
 
-void removeAll(int* heap, int index, int& size) {
-    // Base case: If the heap is empty, stop recursion
-    if (size == 0) {
-        return;
+int removeAll(int* heap, int index) {
+
+  int temps=heap[0];
+  int size=0;
+  while(heap[size]!=0){
+    size++;
+  }
+  //so you dont run of the end of the array
+  size=size-1;
+  int rightNode = index;
+
+  //int temps=heap[0];
+  //cout<<size<<endl;
+  heap[index] = heap[size];
+  heap[size] = 0;
+  
+  //if current node is smaller than right and left
+  while((heap[rightNode] < heap[rightNode * 2]) || (heap[rightNode] < heap[rightNode * 2 + 1])) {
+    //swap current with larger of left/right
+    //current is now the one you swapped with
+    //cout<<heap[rightNode]<<endl;
+    //cout<<heap[rightNode*2]<<endl;
+    //cout<<heap[rightNode*2+1]<<endl;
+    //cout<<"while loop"<<endl;
+    //if left is bigger than right, replace root with left
+    if (heap[rightNode * 2] > heap[rightNode * 2 +1]) {
+      //cout<<"got herre?"<<endl;
+      int temp = heap[rightNode];
+      heap[rightNode] = heap[rightNode*2];
+      heap[rightNode*2] = temp;
+      rightNode = rightNode*2;
     }
 
-    // Move the last element to the current index (root) and reduce size
-    heap[index] = heap[size - 1];
-    size--;
-
-    // Recursive call to heapify and restore heap property
-    int parent = index;
-    int leftChild = 2 * parent + 1;
-    int rightChild = 2 * parent + 2;
-    int smallest = parent;
-
-    //  if leftchild is less than size so we aren't at the end of the tree
-    // and the leftchild of head is less than the smallest one (so
-    //  15
-    //20
-    //   13
-    if (leftChild < size && heap[leftChild] < heap[smallest]) {
-        smallest = leftChild;
+    //same thing but just for right so everything is +1
+    else if (heap[rightNode * 2 +1] > heap[rightNode *2]) {
+      //cout<<"got maybeee?"<<endl;
+      int temp = heap[rightNode];
+      heap[rightNode] = heap[rightNode*2+1];
+      heap[rightNode*2+1] = temp; 
+      rightNode = rightNode*2+1;
     }
-
-    // Compare with right child
-    if (rightChild < size && heap[rightChild] < heap[smallest]) {
-        smallest = rightChild;
-    }
-
-    // If the smallest is not the parent, swap and recursively heapify
-    if (smallest != parent) {
-        swap(heap[parent], heap[smallest]);
-        removeAll(heap, smallest, size);  // Recursively fix the heap
-    }
+  }
+  //dont have to print heap everytime but can
+  //print(heap,0,0,size);
+  return temps; 
+  //print(heap,0,0,size);
 }
