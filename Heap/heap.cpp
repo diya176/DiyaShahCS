d620ed7f254f3b36f8396b6e2d0974a59027e37e
U-BSD\419842 <419842@SHS-B4W2NW3.beaverton.k12.or.us>
2025-02-11 09:36:13 -0800

    Finished heap with comments

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <cstring>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+
+
+/*
+  This program mimics a max heap using an array. It is visualized using tabs to show the children of the roots. From this heap you can delete all the nodes in the heap which effectively sorts the heap. This is done by repeatedly calling the delete root function. The delete root function deletes only the root or parent node. Finally, you can also manually add nodes or put numbers in a file and add that file. 
+  
+  Author: Diya Shah
+  
+  Date: 2/11/2025
+ */
+
+
+//function prototypes 
 void add (int* heap, int &size, int numtoadd);
 void heapify (int* heap, int index);
 void print (int* heap,int pos, int depth, int size);
@@ -10,13 +26,17 @@ int removeAll(int* heap, int index);
 
 
 int main(){
-  int heap[100];
+  //creating the heap and setting the size
+  int heap[101];
   int size=0;
 
-  for(int i = 1; i < 100; i++) {
+  //making everything in the heap a 0
+  for(int i = 1; i < 101; i++) {
     heap[i] = 0;
   }
   //heapify(heap, size-1);
+
+  /*
   int numtoadd=20;
   add(heap,size,numtoadd);
   print(heap,0,0,size);
@@ -53,7 +73,7 @@ int main(){
   print(heap,0,0,size);
   cout<<endl;
   cout<<endl;
-  */
+  
 
   numtoadd=11;
   add(heap,size,numtoadd);
@@ -93,24 +113,85 @@ int main(){
   print(heap,0,0,size);
   cout<<endl;
   cout<<endl;
-  
-  
+
+  */
+
+
+  //running the actual code
   while (true){
     int index=0;
     char input[200];
+    cout<<"Would you like to add MANUALLY, from a FILE, delete the LARGEST node, delete ALL the nodes, or QUIT?"<<endl;
     cin>>input;
-    cout<<input<<endl;
+    //cout<<input<<endl;
 
-    if(strcmp(input,"Largest")==0){
-      cout<<"got here"<<endl;
-      cout<<removeAll(heap,index)<<endl;
+    //manually adding a certain amount of numbers (needs to be less than 100 
+    if (strcmp(input,"MANUALLY")==0){
+      int numofreps=0;
+      //figuring out how many numbers the user wants to add
+      cout<<"How many numbers would you like to add: ";
+      cin>>numofreps;
+
+      //amount of numbers to be added needs to be less than 100
+      if(numofreps>100){
+	cout<<"You can only add less than 100 numbers"<<endl;
+	;
+      }
+      //if it is less than 100
+      else{
+	//add the number and then print out the heap visualizer
+	for (int i=0;i<numofreps;i++){
+	  int numtoadd;
+	  cout<<"Please enter a number: ";
+	  cin>>numtoadd;
+	  add(heap,size,numtoadd);
+	  print(heap,0,0,size);
+	  cout<<endl;
+	  cout<<endl;
+	}
+      }
+      
+      //int numtoadd;
+      //cin>>numtoadd;
+      
     }
-    else if (strcmp(input,"All")==0){
+    //add the numbers in through the file 
+    else if (strcmp(input,"FILE")==0){
+      char fileName[100];
+      //figure out the file name
+      cout << "Please enter your file name including the extension: ";
+      cin>>fileName;
+      cin.ignore();
+      ifstream f;
+      f.open(fileName);
+      int input=0;
+      int index=0;
+      //read the numbers in from the file and add them into the heap
+      //keep changing (incrementing the index)
+      while (f>>input){
+	add(heap,size,input);
+	index++;
+      }
+      f.close();
+      print(heap,0,0,size);
+    }
+    //delete just the root (parent) of the heap 
+    else if(strcmp(input,"LARGEST")==0){
+      //cout<<"got here"<<endl;
+      cout<<removeAll(heap,index)<<endl;
+      //decrease the size by 1 because now we have 1 less value
+      size--;
+    }
+    //delete everything in the heap 
+    else if (strcmp(input,"ALL")==0){
+      //keep calling the removeAll function until we have removed all the values
       while(heap[index] != 0) {
 	cout<<removeAll(heap, index)<<endl;
       }
+      //nothing in the heap now so set the size to 0
+      size=0;
     }
-    else if (strcmp(input, "Quit")==0){
+    else if (strcmp(input, "QUIT")==0){
       exit(0);
     }
 
@@ -121,13 +202,18 @@ int main(){
   //print(heap,0,0,size);
 }
 
+//add the value into the heap
+//increase the size by 1
+//then heapify it (or sort it in normal words)
 void add (int* heap, int &size, int numtoadd){
   heap[size]=numtoadd;
   size++;
   heapify(heap,size-1);
 }
 
+//sort the heap to make sure that all the nodes are in the correct order
 void heapify (int* heap, int index){
+  //parent is the index -1 /2
   int parent=(index-1)/2;
   if(index==0){
     return;
@@ -137,14 +223,17 @@ void heapify (int* heap, int index){
   }
   else{
     //might be parent index;
+    //swap if they are not in the correct order
     swap(heap[parent],heap[index]);
     heapify(heap,parent);
   }
   
 }
 
-
+//way to visualize the heap using tabs 
+//Taken from back board in CS classroom 
 void print (int* heap,int pos, int depth, int size){
+  //need to add 1 because I am starting from 0 and not 1 
   if(pos*2+2<size){
     print(heap,pos*2+2, depth+1, size);
   }
@@ -157,11 +246,14 @@ void print (int* heap,int pos, int depth, int size){
   }
 }
 
+//same function for both removing the root and removing everything 
 int removeAll(int* heap, int index) {
 
+  //returning the root 
   int temps=heap[0];
   //cout<<heap[0]<<endl;
   int sizes=0;
+  //figure out the size of the heap
   while(heap[sizes]!=0){
     sizes++;
   }
